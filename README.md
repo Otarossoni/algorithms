436019454acf179b428b008a4371728ced43dafa
Otarossoni <otarossoni@gmail.com>
2024-03-20 13:40:17 -0300

    feat: first algorithm

---
+++ README.md
@@ -0,0 +1,8 @@
+<div id="header">
+   <p align="center">
+      <img src="/@assets/algorithms-logo.svg" width="80px" align="center" alt="cdp-logo" />
+   <h1 align="center">Algorithms</h1>
+   </p>
+</div>
+
+Focused on saving examples of algorithms studied in languages that I know, or am I studying, or learning.
---
+++ algorithms/binary-search/algorithm.ts
@@ -0,0 +1,26 @@
+function binarySearch<T> (list: T[], target: T): number | null {
+  let low = 0
+  let high = list.length - 1
+
+  while (low <= high) {
+    const medium = Math.floor((low + high) / 2)
+    const value = list[Math.round(medium)]
+
+    if (value === target) {
+      return medium
+    }
+
+    if (value > target) {
+      high = medium - 1
+    } else {
+      low = medium + 1
+    }
+  }
+  
+  return null
+}
+
+const list = [10, 20, 30, 40, 50]
+
+console.log(binarySearch(list, 0))
+console.log(binarySearch(list, 10))
